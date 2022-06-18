bdbea58de1a8c51110efa9618e738b13ef309543
DAVID HO <davidhorph@hotmail.com>
2022-06-18 07:39:09 -0500

    subheader to homepage.js

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { Container, Row } from 'reactstrap'
 import DisplayList from '../features/Display/DisplayList'
-
+import SubHeader from '../Components/SubHeader';
 
 function Homepage() {
     return (
         <Container>
+            <SubHeader current='HOME' />
             <Row className='text-center'>
                 <h1>WELCOME TO THE HOMEPAGE!!!</h1>
             </Row>
