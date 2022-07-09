fb8b6e714160746f426f9e3d27daeabcce6283e4
DAVID HO <davidhorph@hotmail.com>
2022-07-09 10:44:17 -0500

    comment list.

--- src/features/Comments/CommentsList.js
+++ src/features/Comments/CommentsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Col } from 'reactstrap'
+import Error from '../../Components/Error'
+import Loading from '../../Components/Loading'
 import Comment from './Comment'
 import CommentForm from './CommentForm'
 import { selectCommentsByCampsiteId } from './commentsSlice'
@@ -8,8 +10,18 @@ import { selectCommentsByCampsiteId } from './commentsSlice'
 
 function CommentsList({ campsiteId}) {
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector(state => state.comments.errMsg);
 
-    if (comments && comments.length) {
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (errMsg) {
+        return <Error errMsg={errMsg} />
+    }
+
+    if (comments && comments.length > 0) {
         return (
             <Col md='5' className='m-1'>
                 <h5>COMMENTS.</h5>
@@ -24,7 +36,7 @@ function CommentsList({ campsiteId}) {
             </Col>
         )
     }
-    
+
     return (
         <Col md='5' className='m-1'>
             <h5>NO COMMENTS YET.</h5>
